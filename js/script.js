0792ebaa4e9b9fdd2c9270067e1ea6273aafde7a
NeonGaming <85440795+NeonGamingMC@users.noreply.github.com>
2022-03-28 19:23:35 +0900

    finish i guess
    yeah

--- js/script.js
+++ js/script.js
@@ -1,8 +1,47 @@
-/*
 setTimeout(function (){
-    $('div').css({
-        'transition':'0s',
-        'transform': 'translate(0px,0px)'
-    });
-}, 0);
-*/
+    $('.title').css({
+        'opacity' : '100%'
+    })
+    $('.coloredTitle').css({
+        'opacity' : '100%'
+    })
+},800)
+
+setTimeout(function (){
+    $('#c1').css({
+        'opacity' : '100%'
+    })
+},1300)
+
+setTimeout(function (){
+    $('#c2').css({
+        'opacity' : '100%'
+    })
+},1800)
+
+setTimeout(function (){
+    $('.plane').css({
+        'opacity' : '100%'
+    })
+},2400)
+
+setTimeout(function (){
+    $('#txt0').css({
+        'opacity' : '1'
+    })
+},5300)
+setTimeout(function (){
+    $('#txt1').css({
+        'opacity' : '1'
+    })
+},6300)
+setTimeout(function (){
+    $('#txt2').css({
+        'opacity' : '1'
+    })
+},7300)
+setTimeout(function (){
+    $('#txt3').css({
+        'opacity' : '1'
+    })
+},8300)
